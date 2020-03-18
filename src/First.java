1fb22aed91066c46e9455b1ef7aaad841f8558db
kayabay319 <kayabay319@gmail.com>
2020-03-18 16:10:17 -0400

    Project name is changed

--- src/First.java
+++ src/First.java
@@ -4,6 +4,7 @@ public class First {
 	public static void main(String[] args) {
 		System.out.println("Sunny");
 		System.out.println("Yes");
+		System.out.println("Project name is updated");
 
 	}
 
